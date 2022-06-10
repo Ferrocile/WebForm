c8fd9933aef12e60925aa9b7b0a9a3978891c6b2
NUANCE\brian.datz <brian.datz@nuance.com>
2022-06-09 21:07:39 -0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $(function () {
+        $.ajax({
+            url: "https://restcountries.com/v3.1/all",
+            contentType: "application/json",
+            success: function (response) {
+                var r = '<option value="-1">Select a Region</option>';
+                $.each(response, function (index, val) {
+                    r += '<option value="' + val.region + '">' + val.region + '</option>';
+                });
+                $('#regionDropdown').append(r);
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        });
+    });
+
+    $('#submitBtn').click(function () {
+        $('#textarea').prepend('Personal Information:');
+        $('#display_info').append('<p>First Name: ' + $('#firstName').val() + '</p>');
+        $('#display_info').append('<p>Last Name: ' + $('#lastName').val() + '</p>');
+        $('#display_info').append('<p>Email: ' + $('#email').val() + '</p>');
+        $('#display_info').append('<p>Region: ' + $('#regionDropdown').val() + '</p>');
+        $('#display_info').append('<p>Country: ' + $('#countryDropdown').val() + '</p>');
+    });
+
+});
